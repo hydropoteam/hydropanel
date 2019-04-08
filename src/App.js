b3ac484760c98e12e0bbe434420bf6c28d7a660a
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-04-08 13:57:26 +0200

    removing useless files / Created 3 base compos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.jsx';
+import Main from './components/Main.jsx';
+import Footer from './components/Footer.jsx';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Header />
+				<Main />
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
